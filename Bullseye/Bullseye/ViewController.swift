a7467c1c813743b582f058f13a69c4258597efe3
Marvin <marvkipi@gmail.com>
2019-08-06 01:44:03 -0500

    init

---
+++ Bullseye/Bullseye/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Marvin Nikijuluw on 8/6/19.
+//  Copyright © 2019 Marvin Nikijuluw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello world", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
