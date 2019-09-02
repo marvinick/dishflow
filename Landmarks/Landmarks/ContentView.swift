281cb7773ca6c9417434cf1b603dc88a19c4e9d8
Marvin <marvkipi@gmail.com>
2019-09-02 16:12:12 -0500

    init

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Marvin Nikijuluw on 9/2/19.
+//  Copyright © 2019 Marvin Nikijuluw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Yeah Gurl")
+            .font(.title)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Marvin Nikijuluw on 8/25/19.
+//  Copyright © 2019 Marvin Nikijuluw. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
